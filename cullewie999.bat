85ae9f7edda6818fb6646a20901eb4453e75373d
cullewie <fatihvatansever98@gmail.com>
2025-07-21 15:25:31 +0300

    cullewie999.bat

--- cullewie999.bat
+++ cullewie999.bat
@@ -1,6 +1,6 @@
 @echo off
-title CULLEWIE MAX 
-echo [#] Hallediliyor..
+title CULLEWIE MAX
+echo [#] Preparing...
 
 for /f "tokens=4 delims= " %%A in ('powercfg -duplicatescheme 8c5e7fda-e8bf-4a96-9a85-a6e23a8c635c') do set NEWGUID=%%A
 
@@ -11,37 +11,36 @@ if "%NEWGUID%"=="" (
 )
 
 echo [#] New GUID is %NEWGUID%
-powercfg /changename %NEWGUID% "Cullewié 999"
 
-:: CPU %100
+echo CPU 100% set
 powercfg /setacvalueindex %NEWGUID% SUB_PROCESSOR PROCTHROTTLEMIN 100
 powercfg /setdcvalueindex %NEWGUID% SUB_PROCESSOR PROCTHROTTLEMIN 100
 powercfg /setacvalueindex %NEWGUID% SUB_PROCESSOR PROCTHROTTLEMAX 100
 powercfg /setdcvalueindex %NEWGUID% SUB_PROCESSOR PROCTHROTTLEMAX 100
 
-:: Boost Mode agresif
+echo Boost Mode aggressively set
 powercfg /setacvalueindex %NEWGUID% SUB_PROCESSOR PERFBOOSTMODE 5
 powercfg /setdcvalueindex %NEWGUID% SUB_PROCESSOR PERFBOOSTMODE 5
 
-:: Core Parking KAPALI
+echo Core Parking disabled
 powercfg /setacvalueindex %NEWGUID% SUB_PROCESSOR CPMINCORES 100
 powercfg /setdcvalueindex %NEWGUID% SUB_PROCESSOR CPMINCORES 100
 powercfg /setacvalueindex %NEWGUID% SUB_PROCESSOR CPMAXCORES 100
 powercfg /setdcvalueindex %NEWGUID% SUB_PROCESSOR CPMAXCORES 100
 
-:: PCI Express ASPM = OFF
+echo PCI Express ASPM disabled
 powercfg /setacvalueindex %NEWGUID% SUB_PCIEXPRESS ASPM 0
 powercfg /setdcvalueindex %NEWGUID% SUB_PCIEXPRESS ASPM 0
 
-:: USB Seçmeli Askıya Alma = Kapalı
+echo USB Selective Suspend disabled
 powercfg /setacvalueindex %NEWGUID% 2a737441-1930-4402-8d77-b2bebba308a3 48e6b7a6-50f5-4782-a5d4-53bb8f07e226 0
 powercfg /setdcvalueindex %NEWGUID% 2a737441-1930-4402-8d77-b2bebba308a3 48e6b7a6-50f5-4782-a5d4-53bb8f07e226 0
 
-:: Disk Kapanma Süresi = 0 (KAPALI)
+echo Disk idle timeout disabled
 powercfg /setacvalueindex %NEWGUID% SUB_DISK DISKIDLE 0
 powercfg /setdcvalueindex %NEWGUID% SUB_DISK DISKIDLE 0
 
-:: Uyku, Hazırda Bekletme = KAPALI
+echo Sleep and hibernate disabled
 powercfg /setacvalueindex %NEWGUID% SUB_SLEEP STANDBYIDLE 0
 powercfg /setdcvalueindex %NEWGUID% SUB_SLEEP STANDBYIDLE 0
 powercfg /setacvalueindex %NEWGUID% SUB_SLEEP HIBERNATEIDLE 0
@@ -49,20 +48,20 @@ powercfg /setdcvalueindex %NEWGUID% SUB_SLEEP HIBERNATEIDLE 0
 powercfg /setacvalueindex %NEWGUID% SUB_SLEEP HYBRIDSLEEP 0
 powercfg /setdcvalueindex %NEWGUID% SUB_SLEEP HYBRIDSLEEP 0
 
-:: Video (Ekran) kapanma = KAPALI
+echo Video idle timeout disabled
 powercfg /setacvalueindex %NEWGUID% SUB_VIDEO VIDEOIDLE 0
 powercfg /setdcvalueindex %NEWGUID% SUB_VIDEO VIDEOIDLE 0
 
-:: Multimedya - performans
+echo Multimedia performance mode set
 powercfg /setacvalueindex %NEWGUID% 9596fb26-9850-41fd-ac3e-f7c3c00afd4b 03680956-93bc-4294-bba6-4e0f09bb717f 1
 powercfg /setdcvalueindex %NEWGUID% 9596fb26-9850-41fd-ac3e-f7c3c00afd4b 03680956-93bc-4294-bba6-4e0f09bb717f 1
 
-:: Planı aktif et
+echo Activating power plan
 powercfg /setactive %NEWGUID%
 
-:: TimerResolution 0.5ms registry ayarı
+echo TimerResolution 0.5ms registry setting applied
 reg add "HKLM\SYSTEM\CurrentControlSet\Control\Session Manager\kernel" /v "GlobalTimerResolutionRequests" /t REG_DWORD /d 1 /f >nul 2>&1
 
-echo [✓] Plan basariyla uygulandi.
-echo [!] Sistemi yeniden baslat.
+echo [✓] Power plan applied successfully.
+echo [!] Please reboot your system.
 pause
